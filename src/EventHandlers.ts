6b11de61547f21e3d5fc03e52800abff0689c70e
root <root@VENGEANCE>
2024-05-03 18:08:01 -0700

    all contracts loaded in, will start on schema

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -3,26 +3,434 @@
  */
 import {
   MemeContract,
+    Meme_Meme__BuyEntity,
+    Meme_Meme__ClaimEntity,
+    Meme_Meme__MarketOpenedEntity,
+    Meme_Meme__ProtocolFeeEntity,
+    Meme_Meme__ProviderFeeEntity,
+    Meme_Meme__SellEntity,
+    Meme_Meme__StatusFeeEntity,
+    Meme_Meme__StatusUpdatedEntity,
+    Meme_TransferEntity,
+  PreMemeContract,
+    PreMeme_PreMeme__ContributedEntity,
+    PreMeme_PreMeme__RedeemedEntity,
   WaveFrontFactoryContract,
+    WaveFrontFactory_WaveFrontFactory__MemeCreatedEntity,
   WaveFrontRouterContract,
-  DirectoryEntity,
+    WaveFrontRouter_WaveFrontRouter__AffiliateSetEntity,
+    WaveFrontRouter_WaveFrontRouter__BuyEntity,
+    WaveFrontRouter_WaveFrontRouter__SellEntity,
+EventsSummaryEntity
 } from "generated";
-import {
-  WaveFrontFactoryContract_WaveFrontFactory__MemeCreated_handler,
-  WaveFrontFactoryContract_WaveFrontFactory__MemeCreated_handlerAsync,
-  WaveFrontFactoryContract_WaveFrontFactory__MemeCreated_loader,
-} from "generated/src/Handlers.gen";
 
-export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export const FACTORY_ADDRESS = "0x1552b0DCAC344fFA9702Dbafa6EfA5ebEFB62A82";
-export const MULTICALL_ADDRESS = "0xB5ccEA2Ebb813EA818f2571b89A686E137E67889";
+export const GLOBAL_EVENTS_SUMMARY_KEY = "GlobalEventsSummary";
 
-WaveFrontFactoryContract_WaveFrontFactory__MemeCreated_loader(
-  ({ event, context }) => {
-    context.Directory.load(FACTORY_ADDRESS);
-  }
-);
+const INITIAL_EVENTS_SUMMARY: EventsSummaryEntity = {
+  id: GLOBAL_EVENTS_SUMMARY_KEY,
+    meme_Meme__BuyCount: BigInt(0),
+    meme_Meme__ClaimCount: BigInt(0),
+    meme_Meme__MarketOpenedCount: BigInt(0),
+    meme_Meme__ProtocolFeeCount: BigInt(0),
+    meme_Meme__ProviderFeeCount: BigInt(0),
+    meme_Meme__SellCount: BigInt(0),
+    meme_Meme__StatusFeeCount: BigInt(0),
+    meme_Meme__StatusUpdatedCount: BigInt(0),
+    meme_TransferCount: BigInt(0),
+    preMeme_PreMeme__ContributedCount: BigInt(0),
+    preMeme_PreMeme__RedeemedCount: BigInt(0),
+    waveFrontFactory_WaveFrontFactory__MemeCreatedCount: BigInt(0),
+    waveFrontRouter_WaveFrontRouter__AffiliateSetCount: BigInt(0),
+    waveFrontRouter_WaveFrontRouter__BuyCount: BigInt(0),
+    waveFrontRouter_WaveFrontRouter__SellCount: BigInt(0),
+};
 
-WaveFrontFactoryContract_WaveFrontFactory__MemeCreated_handler(
-  ({ event, context }) => {}
-);
+    MemeContract.Meme__Buy.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__Buy.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__BuyCount: currentSummaryEntity.meme_Meme__BuyCount + BigInt(1),
+  };
+
+  const meme_Meme__BuyEntity: Meme_Meme__BuyEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      from: event.params.from      ,
+      to: event.params.to      ,
+      amountIn: event.params.amountIn      ,
+      amountOut: event.params.amountOut      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__Buy.set(meme_Meme__BuyEntity);
+});
+    MemeContract.Meme__Claim.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__Claim.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__ClaimCount: currentSummaryEntity.meme_Meme__ClaimCount + BigInt(1),
+  };
+
+  const meme_Meme__ClaimEntity: Meme_Meme__ClaimEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      account: event.params.account      ,
+      amountBase: event.params.amountBase      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__Claim.set(meme_Meme__ClaimEntity);
+});
+    MemeContract.Meme__MarketOpened.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__MarketOpened.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__MarketOpenedCount: currentSummaryEntity.meme_Meme__MarketOpenedCount + BigInt(1),
+  };
+
+  const meme_Meme__MarketOpenedEntity: Meme_Meme__MarketOpenedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__MarketOpened.set(meme_Meme__MarketOpenedEntity);
+});
+    MemeContract.Meme__ProtocolFee.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__ProtocolFee.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__ProtocolFeeCount: currentSummaryEntity.meme_Meme__ProtocolFeeCount + BigInt(1),
+  };
+
+  const meme_Meme__ProtocolFeeEntity: Meme_Meme__ProtocolFeeEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      account: event.params.account      ,
+      amountBase: event.params.amountBase      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__ProtocolFee.set(meme_Meme__ProtocolFeeEntity);
+});
+    MemeContract.Meme__ProviderFee.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__ProviderFee.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__ProviderFeeCount: currentSummaryEntity.meme_Meme__ProviderFeeCount + BigInt(1),
+  };
+
+  const meme_Meme__ProviderFeeEntity: Meme_Meme__ProviderFeeEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      account: event.params.account      ,
+      amountBase: event.params.amountBase      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__ProviderFee.set(meme_Meme__ProviderFeeEntity);
+});
+    MemeContract.Meme__Sell.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__Sell.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__SellCount: currentSummaryEntity.meme_Meme__SellCount + BigInt(1),
+  };
+
+  const meme_Meme__SellEntity: Meme_Meme__SellEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      from: event.params.from      ,
+      to: event.params.to      ,
+      amountIn: event.params.amountIn      ,
+      amountOut: event.params.amountOut      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__Sell.set(meme_Meme__SellEntity);
+});
+    MemeContract.Meme__StatusFee.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__StatusFee.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__StatusFeeCount: currentSummaryEntity.meme_Meme__StatusFeeCount + BigInt(1),
+  };
+
+  const meme_Meme__StatusFeeEntity: Meme_Meme__StatusFeeEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      account: event.params.account      ,
+      amountBase: event.params.amountBase      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__StatusFee.set(meme_Meme__StatusFeeEntity);
+});
+    MemeContract.Meme__StatusUpdated.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Meme__StatusUpdated.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_Meme__StatusUpdatedCount: currentSummaryEntity.meme_Meme__StatusUpdatedCount + BigInt(1),
+  };
+
+  const meme_Meme__StatusUpdatedEntity: Meme_Meme__StatusUpdatedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      account: event.params.account      ,
+      status: event.params.status      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Meme__StatusUpdated.set(meme_Meme__StatusUpdatedEntity);
+});
+    MemeContract.Transfer.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    MemeContract.Transfer.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    meme_TransferCount: currentSummaryEntity.meme_TransferCount + BigInt(1),
+  };
+
+  const meme_TransferEntity: Meme_TransferEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      from: event.params.from      ,
+      to: event.params.to      ,
+      value: event.params.value      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.Meme_Transfer.set(meme_TransferEntity);
+});
+    PreMemeContract.PreMeme__Contributed.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    PreMemeContract.PreMeme__Contributed.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    preMeme_PreMeme__ContributedCount: currentSummaryEntity.preMeme_PreMeme__ContributedCount + BigInt(1),
+  };
+
+  const preMeme_PreMeme__ContributedEntity: PreMeme_PreMeme__ContributedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      meme: event.params.meme      ,
+      account: event.params.account      ,
+      amount: event.params.amount      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.PreMeme_PreMeme__Contributed.set(preMeme_PreMeme__ContributedEntity);
+});
+    PreMemeContract.PreMeme__Redeemed.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    PreMemeContract.PreMeme__Redeemed.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    preMeme_PreMeme__RedeemedCount: currentSummaryEntity.preMeme_PreMeme__RedeemedCount + BigInt(1),
+  };
+
+  const preMeme_PreMeme__RedeemedEntity: PreMeme_PreMeme__RedeemedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      meme: event.params.meme      ,
+      account: event.params.account      ,
+      amount: event.params.amount      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.PreMeme_PreMeme__Redeemed.set(preMeme_PreMeme__RedeemedEntity);
+});
+    WaveFrontFactoryContract.WaveFrontFactory__MemeCreated.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    WaveFrontFactoryContract.WaveFrontFactory__MemeCreated.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    waveFrontFactory_WaveFrontFactory__MemeCreatedCount: currentSummaryEntity.waveFrontFactory_WaveFrontFactory__MemeCreatedCount + BigInt(1),
+  };
+
+  const waveFrontFactory_WaveFrontFactory__MemeCreatedEntity: WaveFrontFactory_WaveFrontFactory__MemeCreatedEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      meme: event.params.meme      ,
+      preMeme: event.params.preMeme      ,
+      name: event.params.name      ,
+      symbol: event.params.symbol      ,
+      uri: event.params.uri      ,
+      account: event.params.account      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.WaveFrontFactory_WaveFrontFactory__MemeCreated.set(waveFrontFactory_WaveFrontFactory__MemeCreatedEntity);
+});
+    WaveFrontRouterContract.WaveFrontRouter__AffiliateSet.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    WaveFrontRouterContract.WaveFrontRouter__AffiliateSet.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    waveFrontRouter_WaveFrontRouter__AffiliateSetCount: currentSummaryEntity.waveFrontRouter_WaveFrontRouter__AffiliateSetCount + BigInt(1),
+  };
+
+  const waveFrontRouter_WaveFrontRouter__AffiliateSetEntity: WaveFrontRouter_WaveFrontRouter__AffiliateSetEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      account: event.params.account      ,
+      affiliate: event.params.affiliate      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.WaveFrontRouter_WaveFrontRouter__AffiliateSet.set(waveFrontRouter_WaveFrontRouter__AffiliateSetEntity);
+});
+    WaveFrontRouterContract.WaveFrontRouter__Buy.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    WaveFrontRouterContract.WaveFrontRouter__Buy.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    waveFrontRouter_WaveFrontRouter__BuyCount: currentSummaryEntity.waveFrontRouter_WaveFrontRouter__BuyCount + BigInt(1),
+  };
+
+  const waveFrontRouter_WaveFrontRouter__BuyEntity: WaveFrontRouter_WaveFrontRouter__BuyEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      meme: event.params.meme      ,
+      account: event.params.account      ,
+      affiliate: event.params.affiliate      ,
+      amountIn: event.params.amountIn      ,
+      amountOut: event.params.amountOut      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.WaveFrontRouter_WaveFrontRouter__Buy.set(waveFrontRouter_WaveFrontRouter__BuyEntity);
+});
+    WaveFrontRouterContract.WaveFrontRouter__Sell.loader(({ event, context }) => {
+  context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
+});
+
+    WaveFrontRouterContract.WaveFrontRouter__Sell.handler(({ event, context }) => {
+  const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
+
+  const currentSummaryEntity: EventsSummaryEntity =
+    summary ?? INITIAL_EVENTS_SUMMARY;
+
+  const nextSummaryEntity = {
+    ...currentSummaryEntity,
+    waveFrontRouter_WaveFrontRouter__SellCount: currentSummaryEntity.waveFrontRouter_WaveFrontRouter__SellCount + BigInt(1),
+  };
+
+  const waveFrontRouter_WaveFrontRouter__SellEntity: WaveFrontRouter_WaveFrontRouter__SellEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+      meme: event.params.meme      ,
+      account: event.params.account      ,
+      amountIn: event.params.amountIn      ,
+      amountOut: event.params.amountOut      ,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+  };
+
+  context.EventsSummary.set(nextSummaryEntity);
+  context.WaveFrontRouter_WaveFrontRouter__Sell.set(waveFrontRouter_WaveFrontRouter__SellEntity);
+});
--- test/Test.ts
+++ test/Test.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { 
   TestHelpers,
   EventsSummaryEntity,
-  Meme_Meme__BorrowEntity
+  Meme_Meme__BuyEntity
 } from "generated";
 const { MockDb, Meme, Addresses } = TestHelpers;
 
@@ -11,33 +11,24 @@ import { GLOBAL_EVENTS_SUMMARY_KEY } from "../src/EventHandlers";
 
 const MOCK_EVENTS_SUMMARY_ENTITY: EventsSummaryEntity = {
   id: GLOBAL_EVENTS_SUMMARY_KEY,
-  meme_Meme__BorrowCount: BigInt(0),
-  meme_Meme__BurnCount: BigInt(0),
   meme_Meme__BuyCount: BigInt(0),
   meme_Meme__ClaimCount: BigInt(0),
-  meme_Meme__DonationCount: BigInt(0),
-  meme_Meme__FeesCount: BigInt(0),
+  meme_Meme__MarketOpenedCount: BigInt(0),
   meme_Meme__ProtocolFeeCount: BigInt(0),
   meme_Meme__ProviderFeeCount: BigInt(0),
-  meme_Meme__RepayCount: BigInt(0),
-  meme_Meme__ReserveBurnCount: BigInt(0),
   meme_Meme__SellCount: BigInt(0),
   meme_Meme__StatusFeeCount: BigInt(0),
   meme_Meme__StatusUpdatedCount: BigInt(0),
   meme_TransferCount: BigInt(0),
+  preMeme_PreMeme__ContributedCount: BigInt(0),
+  preMeme_PreMeme__RedeemedCount: BigInt(0),
   waveFrontFactory_WaveFrontFactory__MemeCreatedCount: BigInt(0),
   waveFrontRouter_WaveFrontRouter__AffiliateSetCount: BigInt(0),
   waveFrontRouter_WaveFrontRouter__BuyCount: BigInt(0),
-  waveFrontRouter_WaveFrontRouter__ClaimFeesCount: BigInt(0),
-  waveFrontRouter_WaveFrontRouter__ContributedCount: BigInt(0),
-  waveFrontRouter_WaveFrontRouter__MarketOpenedCount: BigInt(0),
-  waveFrontRouter_WaveFrontRouter__MemeCreatedCount: BigInt(0),
-  waveFrontRouter_WaveFrontRouter__RedeemedCount: BigInt(0),
   waveFrontRouter_WaveFrontRouter__SellCount: BigInt(0),
-  waveFrontRouter_WaveFrontRouter__StatusUpdatedCount: BigInt(0),
 };
 
-describe("Meme contract Meme__Borrow event tests", () => {
+describe("Meme contract Meme__Buy event tests", () => {
   // Create mock db
   const mockDbInitial = MockDb.createMockDb();
 
@@ -46,10 +37,12 @@ describe("Meme contract Meme__Borrow event tests", () => {
     MOCK_EVENTS_SUMMARY_ENTITY
   );
 
-  // Creating mock Meme contract Meme__Borrow event
-  const mockMemeMeme__BorrowEvent = Meme.Meme__Borrow.createMockEvent({
-    account: Addresses.defaultAddress,
-    amountBase: 0n,
+  // Creating mock Meme contract Meme__Buy event
+  const mockMemeMeme__BuyEvent = Meme.Meme__Buy.createMockEvent({
+    from: Addresses.defaultAddress,
+    to: Addresses.defaultAddress,
+    amountIn: 0n,
+    amountOut: 0n,
     mockEventData: {
       chainId: 1,
       blockNumber: 0,
@@ -63,29 +56,31 @@ describe("Meme contract Meme__Borrow event tests", () => {
   });
 
   // Processing the event
-  const mockDbUpdated = Meme.Meme__Borrow.processEvent({
-    event: mockMemeMeme__BorrowEvent,
+  const mockDbUpdated = Meme.Meme__Buy.processEvent({
+    event: mockMemeMeme__BuyEvent,
     mockDb: mockDbFinal,
   });
 
-  it("Meme_Meme__BorrowEntity is created correctly", () => {
+  it("Meme_Meme__BuyEntity is created correctly", () => {
     // Getting the actual entity from the mock database
-    let actualMemeMeme__BorrowEntity = mockDbUpdated.entities.Meme_Meme__Borrow.get(
-      mockMemeMeme__BorrowEvent.transactionHash +
-        mockMemeMeme__BorrowEvent.logIndex.toString()
+    let actualMemeMeme__BuyEntity = mockDbUpdated.entities.Meme_Meme__Buy.get(
+      mockMemeMeme__BuyEvent.transactionHash +
+        mockMemeMeme__BuyEvent.logIndex.toString()
     );
 
     // Creating the expected entity
-    const expectedMemeMeme__BorrowEntity: Meme_Meme__BorrowEntity = {
+    const expectedMemeMeme__BuyEntity: Meme_Meme__BuyEntity = {
       id:
-        mockMemeMeme__BorrowEvent.transactionHash +
-        mockMemeMeme__BorrowEvent.logIndex.toString(),
-      account: mockMemeMeme__BorrowEvent.params.account,
-      amountBase: mockMemeMeme__BorrowEvent.params.amountBase,
+        mockMemeMeme__BuyEvent.transactionHash +
+        mockMemeMeme__BuyEvent.logIndex.toString(),
+      from: mockMemeMeme__BuyEvent.params.from,
+      to: mockMemeMeme__BuyEvent.params.to,
+      amountIn: mockMemeMeme__BuyEvent.params.amountIn,
+      amountOut: mockMemeMeme__BuyEvent.params.amountOut,
       eventsSummary: "GlobalEventsSummary",
     };
     // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualMemeMeme__BorrowEntity, expectedMemeMeme__BorrowEntity, "Actual MemeMeme__BorrowEntity should be the same as the expectedMemeMeme__BorrowEntity");
+    assert.deepEqual(actualMemeMeme__BuyEntity, expectedMemeMeme__BuyEntity, "Actual MemeMeme__BuyEntity should be the same as the expectedMemeMeme__BuyEntity");
   });
 
   it("EventsSummaryEntity is updated correctly", () => {
@@ -97,9 +92,9 @@ describe("Meme contract Meme__Borrow event tests", () => {
     // Creating the expected entity
     const expectedEventsSummaryEntity: EventsSummaryEntity = {
       ...MOCK_EVENTS_SUMMARY_ENTITY,
-      meme_Meme__BorrowCount: MOCK_EVENTS_SUMMARY_ENTITY.meme_Meme__BorrowCount + BigInt(1),
+      meme_Meme__BuyCount: MOCK_EVENTS_SUMMARY_ENTITY.meme_Meme__BuyCount + BigInt(1),
     };
     // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualEventsSummaryEntity, expectedEventsSummaryEntity, "Actual MemeMeme__BorrowEntity should be the same as the expectedMemeMeme__BorrowEntity");
+    assert.deepEqual(actualEventsSummaryEntity, expectedEventsSummaryEntity, "Actual MemeMeme__BuyEntity should be the same as the expectedMemeMeme__BuyEntity");
   });
 });
